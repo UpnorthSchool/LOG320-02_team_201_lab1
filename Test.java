2c581cf7f3f948395d1ef28073a6749ce4f891b5
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2026-01-27 13:27:34 -0500

    test du display

--- Test.java
+++ Test.java
@@ -1,5 +1,16 @@
 public class Test {
    public static void main(String []args) {
-      System.out.println("TicTacToe!");
+        System.out.println("TicTacToe!");
+        Board board = new Board();
+      //board.play(new Move(0,0),Mark.O);
+      //board.play(new Move(1,0),Mark.X);
+      //board.play(new Move(2,0),Mark.O);
+      //board.play(new Move(0,1),Mark.X);
+      //board.play(new Move(1,1),Mark.O);
+      //board.play(new Move(2,1),Mark.X);
+      //board.play(new Move(0,2),Mark.O);
+      //board.play(new Move(1,2),Mark.X);
+      //board.play(new Move(2,2),Mark.O);
+        board.display();
    }
 }
